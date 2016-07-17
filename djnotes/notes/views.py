2cee6a85f55078a5594b9b059157a0bc1154b5be
jasperthon <japer.guerrero1996@gmail.com>
2016-07-17 13:33:42 -0700

    Initial commit

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import View
+
+from .models import	Note
+from .forms import NoteForm
 # Create your views here.
 class Index(View):
 
 	template_name = 'notes/index.html'
 
 	def get_context_data(self):
-		context = {}
+		context = {
+		'notes':Note.objects.order_by('-id'),
+
+		}
 		return context
 
 	def get(self, request, *args, **kwargs):
 		context = self.get_context_data()
+		return render(request, self.template_name, context)
+	def post(self, request, *args, **kwargs):
+		form = NoteForm(request.POST)
+		context = self.get_context_data()
+		if form.is_valid():
+			form.save()
 		return render(request, self.template_name, context)

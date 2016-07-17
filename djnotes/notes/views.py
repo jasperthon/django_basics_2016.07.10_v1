da042ff658a39ad812b92aa2ec3e883bb8ea56b6
jasperthon <japer.guerrero1996@gmail.com>
2016-07-17 15:59:01 -0700

    Initial commit

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import Http404
+from django.shortcuts import render, redirect
 
 from .models import	Note
 from .forms import NoteForm
@@ -24,3 +26,41 @@ class Index(View):
 		if form.is_valid():
 			form.save()
 		return render(request, self.template_name, context)
+
+
+class Update(View):
+	template_name = 'notes/update.html'
+
+	def get_context_data(self, **kwargs):
+		context = {}
+		try:
+			note_id = kwargs.get('pk', 0)
+			note = Note.objects.get(id=note_id)
+		except Note.DoesNotExist:
+			raise Http404
+		else:
+			context['note'] = note
+			return context
+
+	def get(self, request, *args, **kwargs):
+		context = self.get_context_data(**kwargs)
+		return render(request, self.template_name, context)
+
+	def post(self, request, *args, **kwargs):
+		context = self.get_context_data(**kwargs)
+		form = NoteForm(request.POST, instance=context.get('note'))
+		if form.is_valid():
+			form.save()
+		return render(request, self.template_name, context)
+
+class Delete(Update):
+	template_name = 'notes/delete.html'
+
+	def post(self, request, *args, **kwargs):
+		context = self.get_context_data(**kwargs)
+		note = context.get('note')
+		note.delete()
+
+		return redirect('index')
+
+		
